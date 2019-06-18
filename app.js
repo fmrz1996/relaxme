a29b343c0da94ce79b54381f712b9c2ebfc1769c
unknown <fmrz1996@gmail.com>
2019-06-18 19:57:31 -0400

    playlists working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var landscape = document.getElementById("landscape");
 var playButton = document.getElementById("playButton");
 var menuButton = document.getElementById("menuButton");
 var lsVideo = document.getElementById("lsVideo");
+var footer = document.getElementById("footer");
 var timer;
 var fadeInBuffer = false;
 var isPlayingVideo = true;
@@ -13,6 +14,8 @@ function menuFunction() {
   info.classList.add('opacity-on');
   logo.classList.remove('opacity-off');
   logo.classList.add('opacity-on');
+  footer.classList.remove('opacity-off');
+  footer.classList.add('opacity-on');
   landscape.classList.remove('opacity-on');
 }
 
@@ -22,6 +25,8 @@ function lsFunction() {
   info.classList.add('opacity-off');
   logo.classList.remove('opacity-on');
   logo.classList.add('opacity-off');
+  footer.classList.remove('opacity-on');
+  footer.classList.add('opacity-off');
 }
 
 $(document).mousemove(function(){
@@ -54,12 +59,14 @@ timer = setTimeout(
 function playFunction() {
     playButton.classList.add('init-opacity-off');
     logo.classList.add('init-opacity-off');
+    footer.classList.add('init-opacity-off');
     setTimeout(
       function(){
         playButton.classList.add('d-none');
         landscape.classList.add('opacity-on');
         info.classList.add('opacity-off');
         logo.classList.add('opacity-off');
+        footer.classList.add('opacity-off');
         setTimeout(
           function(){
             document.getElementById("sectionMusic").innerHTML = "<div id='videoMusic'></div>";
@@ -70,7 +77,9 @@ function playFunction() {
             document.getElementById("player").classList.remove('d-none');
             menuButton.classList.remove('d-none');
             logo.classList.add('logo');
+            footer.classList.add('fade-in');
             logo.classList.remove('init-opacity-off');
+            footer.classList.remove('init-opacity-off');
 
             // 2. This code loads the IFrame Player API code asynchronously.
             var tag = document.createElement('script');
@@ -86,17 +95,27 @@ function playFunction() {
 //    after the API code downloads._j5QY5LbCOM
 var playerMusic;
 var playerFx;
+var randomIndexMusic = Math.floor(Math.random() * 23);
+var randomIndexFx = Math.floor(Math.random() * 113);
 function onYouTubeIframeAPIReady() {
     playerMusic = new YT.Player('videoMusic', {
         width: '100%',
-        videoId: 'IPSRJ0CBqBw',
+        playerVars: {
+          listType:'playlist',
+          list: 'PLDMVPWmf2MSVR2M_krZ27xyisejTG-tQp',
+          index: randomIndexMusic
+        },
         events: {
             'onReady': onPlayerMusicReady
         }
     });
     playerFx = new YT.Player('videoFx', {
         width: '100%',
-        videoId: '_j5QY5LbCOM',
+        playerVars: {
+          listType:'playlist',
+          list: 'PLfy5ErD61bt9GB5QY1NWKVEQqcxdtI-Xw',
+          index: randomIndexFx
+        },
         events: {
             'onReady': onPlayerFxReady
         }
@@ -105,13 +124,17 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerMusicReady(event) {
-    event.target.setVolume(95);
+    event.target.setShuffle({'shufflePlaylist': true});
+    event.target.setLoop({'loopPlaylists': true});
+    event.target.setVolume(80);
     event.target.seekTo(0);
     event.target.playVideo();
 }
 
 function onPlayerFxReady(event) {
-    event.target.setVolume(60);
+    event.target.setLoop({'loopPlaylists': true});
+    event.target.setShuffle({'shufflePlaylist': true});
+    event.target.setVolume(95);
     event.target.seekTo(30);
     event.target.playVideo();
 }
